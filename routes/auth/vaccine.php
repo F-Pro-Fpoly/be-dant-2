2ec93056c64beeb9200325957ce1c2fdb9591a28
Thanh Phu <phuly4795@gmail.com>
2022-09-29 15:27:34 +0700

    done CRUD vaccine

---
+++ routes/auth/vaccine.php
@@ -0,0 +1,9 @@
+<?php
+    $api -> group(['prefix' => 'vaccine', 'middleware' => 'role:admin'], function ($api) {
+        $api->get('/list', 'VaccineController@listVaccine');
+        $api->post('/add', 'VaccineController@addVaccine');
+        $api->put('/edit/{id:[0-9]+}', 'VaccineController@updateVaccine');
+        $api->delete('/delete/{id:[0-9]+}', 'VaccineController@deleteVaccine');
+    });
+
+?>
--- routes/auth/web_auth.php
+++ routes/auth/web_auth.php
@@ -6,5 +6,5 @@ $api -> get('/', function() {
 
 require_once __DIR__."/user.php";
 require_once __DIR__."/sick.php";
-
+require_once __DIR__."/vaccine.php";
 ?>
