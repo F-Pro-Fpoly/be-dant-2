1e312852e5ef881b64e328f30aee072567f97e1c
Van Phan <phanvan0322@gmail.com>
2022-10-06 11:26:35 +0700

    update be

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Dingo\Api\Http\Request as HttpRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+
 class ExampleController extends Controller
 {
     /**
@@ -14,5 +18,9 @@ class ExampleController extends Controller
         //
     }
 
-    //
+    public function index(Request $request) {
+        $input = $request->all();
+        $name = Arr::get($input, 'name', null);
+        return response()->json(['name' => $name], 200);
+    }
 }
