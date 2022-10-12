e36f1451969d929afe0aa7e80281ec744dbdd2e4
Thanh Phu <phuly4795@gmail.com>
2022-10-12 11:27:57 +0700

    fix chuyên khoa

--- app/Http/Controllers/SpecialistController.php
+++ app/Http/Controllers/SpecialistController.php
@@ -2,37 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Auth;
 use App\Http\Transformer\Specialist\SpecialistTransformer;
 use App\Http\Validators\Specialist\InsertSpecialistValidate;
-use App\Http\Validators\Specialist\UpdateSpecialistValidate;
 use App\Models\Specialist;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Validator;
+
 class SpecialistController extends BaseController
 {
        // add
        public function addSpecialist(Request $request){
+       
         $input = $request->all();
         (new InsertSpecialistValidate($input));
+
         try{
-            $specialist = Specialist::create([
-                'code' => $request->code,
-                'name' => $request->name,
-                'slug' => "/specialist/".$request->slug,
-                'description' => $request->description,
-                'created_by' => auth()->user()->id,
+            Specialist::create([
+                'code' => $input['code'],
+                'name' => $input['name'],
+                'slug' => Str::slug($input['name']),
+                'description' => $input['description'],
+                "created_by" => auth()->user()->id
             ]);
 
-            $arrRes = [
-                'errCode' => 0,
-                'message' => "Thêm thành công",
-                'specialist' => [$specialist]
-            ];
-            return response()->json($arrRes, 200);
-        }
-        catch(\Throwable $th){
+                return response()->json([
+                    'status' => 200,
+                    'message' => "Thêm chuyên khoa thành công"
+            ], 200);
+
+        } catch(\Throwable $th){
             return response()->json(
                 [
                     'status' => 500,
@@ -40,56 +39,44 @@ class SpecialistController extends BaseController
                 ],500
             );
         }
-    }
-       
-    // select all
-    public function listSpecialist(){
-        $specialist = Specialist::all();
-        return response()->json([
-            'message' => "Truy xuất thành công",
-            'specialist' => $specialist, 200
-        ]);
-    }
-    //select ID
-    public function listSpecialist_ID($id){
-        $specialist = Specialist::find($id);
-        return response()->json([
-            'message' => "Truy xuất thành công",
-            'specialist' => $specialist, 200
-        ]);
 
+    }
+    // select all
+    public function listSpecialist(Request $request){
+        $input = $request->all();
+        $Specialist = new Specialist();
+        $data = $Specialist->searchSpecialist($input);
+        return $this->response->paginator($data, new SpecialistTransformer);
     }
    
     // update
     public function updateSpecialist(Request $request, $id){
-        $specialist = Specialist::find($id);
         $input = $request->all();
-        (new UpdateSpecialistValidate($input));          
-
-        try{
-            $specialist->update([
-                'code' => $request->code,
-                'name' => $request->name,
-                'slug' => $request->slug,
-                'description' => $request->description,
-                'updated_by' => auth()->user()->id
-            ]);
-
-            $arrRes = [
-                'errCode' => 0,
-                'message' => "Update thành công",
-                'data' => [$request->all(), $specialist]
-            ];
-        } catch(\Throwable $th){
-            $arrRes = [
-                'errCode' => 0,
-                'message' => "Lỗi phía server",
-                'data' => $th->getMessage()
-            ];
-            return response()->json($arrRes, 500);
-        }
-        return response()->json($arrRes, 200);
+        (new InsertSpecialistValidate($input));
+ 
+       try {
+             $data = Specialist::find($id);
+             $data->update([
+                 'code' => $input['code'],
+                 'name' => $input['name'],
+                 'slug' => Str::slug($input['name']),
+                 'description' => $input['description'],
+                 'updated_by' => auth()->user()->id
+             ]);
+             return response()->json([
+                 'status' => 200,
+                 'message' => "Cập nhật chuyên khoa thành công"
+            ], 200);
+       } catch (\Throwable $th) {
+         return response()->json(
+             [
+                 'status' => 500,
+                 'message' => $th->getMessage() 
+             ],500
+             );
+       }
     }
+    // delete
     public function deleteSpecialist($id){
         try {
             $data = Specialist::find($id);
@@ -100,15 +87,16 @@ class SpecialistController extends BaseController
             return response()->json([
                 'status' => 200,
                 'message' => "Xóa chuyên khoa thành công"
-            ], 200);
-        } catch (\Throwable $th) {
-            return response()->json(
-                [
-                    'status' => 500,
-                    'message' => $th->getMessage() 
-                ],500
-                );
-            }
+        ], 200);
+       } catch (\Throwable $th) {
+        return response()->json(
+            [
+                'status' => 500,
+                'message' => $th->getMessage() 
+            ],500
+            );
+       }
+
     }
 
 }
