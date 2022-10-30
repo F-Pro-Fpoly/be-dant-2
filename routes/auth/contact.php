44d57844271850e94d03e304aa424e4fe05a269a
Thinh <thjh34316@gmail.com>
2022-10-30 14:53:56 +0700

    list

--- routes/auth/contact.php
+++ routes/auth/contact.php
@@ -1,6 +1,7 @@
 <?php
     $api -> group(['prefix' => 'contact', 'middleware' => 'role:admin'], function ($api) {
         $api->post('/add', 'ContactController@addContact');
+        $api->get('/list', 'ContactController@listContact');
         $api->delete('/delete/{id:[0-9]+}', 'ContactController@deleteContact');
 
     });
