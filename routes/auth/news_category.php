bc8f637b38744ae337245945674cbb5224f29126
hacklord2022 <huydnmpc01663@fpt.edu.vn>
2022-11-07 15:16:14 +0700

    Huy|News-News_category-Controller/Model/Validate/Transformer/Routes|FixV1.1

--- routes/auth/news_category.php
+++ routes/auth/news_category.php
@@ -2,8 +2,8 @@
     $api -> group(['prefix' => 'news_category', 'middleware' => 'role:admin'], function ($api) {
         $api->get('/list', 'News_categoryController@listNews_category');
         $api->post('/add', 'News_categoryController@addNews_category');
-        $api->put('/edit/{id:[0-9]+}', 'NewsController_category@updateNews_category');
-        $api->delete('/delete/{id:[0-9]+}', 'NewsController_category@deleteNews_category');
+        $api->put('/edit/{id:[0-9]+}', 'News_categoryController@updateNews_category');
+        $api->delete('/delete/{id:[0-9]+}', 'News_categoryController@deleteNews_category');
     });
 
 ?>
