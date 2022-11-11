fdfe7e91207e547d0aaf9633edb446d0970ee791
lockoi <locntpc01840@fpt.edu.vn>
2022-11-11 17:47:03 +0700

    huy

--- routes/normal/news.php
+++ routes/normal/news.php
@@ -4,7 +4,7 @@
         $api->get('list-news-all', 'NewsController@listNews_all');
         $api->get('/list-news', 'News_categoryController@getNews');
         $api->get('/list-news-in-category/{id:[0-9]+}', 'News_categoryController@getNewsInCategory');
-        $api->get('/news-detail/{id:[0-9]+}', 'NewsController@getNewsID');
+        $api->get('/news-detail/{id}', 'NewsController@getNewsID');
         $api->get('/news-categoryID/{id:[0-9]+}', 'News_categoryController@getNews_categoryID');
         $api->get('/featured', 'NewsController@getNews_featured');
         $api->get('/news-new', 'NewsController@getNews_new');
