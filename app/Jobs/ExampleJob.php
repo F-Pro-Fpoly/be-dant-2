ded4ab2cf2a8c9e11a7ba151bdba5f522181cce3
hackerlord2022 <huydnmpc01663@fpt.edu.vn>
2022-11-24 09:46:30 +0700

    Queue?2

--- app/Jobs/ExampleJob.php
+++ app/Jobs/ExampleJob.php
@@ -2,17 +2,8 @@
 
 namespace App\Jobs;
 
-use App\Models\Newsletter;
-use App\Models\News;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
-
 class ExampleJob extends Job
 {
-    use InteractsWithQueue, Queueable, SerializesModels;
     /**
      * Create a new job instance.
      *
@@ -31,20 +22,5 @@ class ExampleJob extends Job
     public function handle()
     {
         //
-        $title_mail = "Tin sốt dẻo";
-        $data = [];
-        $dataNewsletter = Newsletter::all();
-        $dataNews = News::where('status', 1)->orderBy('created_at', 'DESC')->first();
-        foreach($dataNewsletter as $send){
-            $data['email'][] = $send->email;
-        }
-
-        Mail::send('email.Newsletter', compact('dataNews'), function ($messager) use ($title_mail, $data){
-            $messager->to($data['email'])->subject($title_mail);
-            $messager->from($data['email'], $title_mail);
-        });
-        return response()->json([
-            "message" => "Gửi mail thành công"
-        ], 200);
     }
 }
