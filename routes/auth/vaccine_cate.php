b215d3789846c6eba0948f9e279bc63e13746e25
Van Phan <phanvan0322@gmail.com>
2022-11-25 21:51:10 +0700

    ok

--- routes/auth/vaccine_cate.php
+++ routes/auth/vaccine_cate.php
@@ -1,6 +1,7 @@
 <?php
 $api->group(['middleware' => 'role:admin'], function($api) {
     $api->get('/vaccine-category/list', 'VaccineCategoryController@list');
+    $api->get('/vaccine-category/{id}', 'VaccineCategoryController@show');
     $api->post('/vaccine-category/create', 'VaccineCategoryController@create_vaccine_category');
     $api->put('/vaccine-category/update/{id}', 'VaccineCategoryController@update');
 });
