1ecc6c17da9c19d6b2e0b27cc7b844265a261ca1
hackerlord2022 <huydnmpc01663@fpt.edu.vn>
2022-11-26 15:38:29 +0700

    AvatarDoctor_profileV2.1

--- app/Http/Transformer/Doctor_profile/Doctor_profileTransformer.php
+++ app/Http/Transformer/Doctor_profile/Doctor_profileTransformer.php
@@ -22,6 +22,7 @@ class Doctor_profileTransformer extends TransformerAbstract
             'experience' => $doctor_profile->experience,
             'created_at' => $doctor_profile->created_at->format('d-m-Y'),
             'doctor_name' => $doctor_profile->user->name,
+            'doctor_avatar' => $doctor_profile->user->avatar,
             'specialists_name' => $doctor_profile->user->specailist_id // Chưa lấy được specialist name
         ];
     }
--- routes/normal/web_normal.php
+++ routes/normal/web_normal.php
@@ -17,4 +17,5 @@ require_once __DIR__."/contact.php";
 require_once __DIR__."/booking.php";
 require_once __DIR__."/file.php";
 require_once __DIR__."/newsletter.php";
+require_once __DIR__."/doctor_profile.php";
 ?>
