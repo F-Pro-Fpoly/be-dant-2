897796e345aaedd8cefdf1e7ab7566c3ed6b73c3
Thanh Phu <phuly4795@gmail.com>
2022-11-28 18:56:32 +0700

    a

--- routes/auth/booking.php
+++ routes/auth/booking.php
@@ -6,11 +6,13 @@
         $api->post('/add', 'BookingController@addBooking');
         $api->put('/edit/{id:[0-9]+}', 'BookingController@updateBooking');
         $api->put('/updateBooking/{id:[0-9]+}', 'BookingController@updateBookingDoctor');
-        $api->get('/detail/{id:[0-9]+}', 'BookingController@detailBooking');
+ 
         $api->delete('/delete/{id:[0-9]+}', 'BookingController@deleteBooking');
     });
     $api -> group(['prefix' => 'booking', 'middleware' => 'auth'], function ($api) {
         $api->get('/mylist/user_id={id:[0-9]+}', 'BookingController@listMyBooking');
+        $api->get('/detail/{id:[0-9]+}', 'BookingController@detailBooking');
+        $api->put('/updateCancel/{id:[0-9]+}', 'BookingController@cancelBooking');
     });
 
 ?>
