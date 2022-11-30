be65d87a00b528f5a34293aa40cd39242974ab01
hackerlord2022 <huydnmpc01663@fpt.edu.vn>
2022-11-30 16:53:07 +0700

    Huy-statisticChart

--- routes/auth/count.php
+++ routes/auth/count.php
@@ -1,3 +1,9 @@
 <?php 
         $api->get('/count', 'CountController@count');
 ?>
+<?php
+    $api -> group(['prefix' => 'statistic' ,  'middleware' => 'role:admin' ], function ($api) {
+        $api->get('/list', 'CountController@getStatistic');
+        $api->get('/chart', 'CountController@getStatisticChart');
+    });
+?>
